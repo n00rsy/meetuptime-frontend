88cc10a976aafa50597383b4044c38de5b159a7d
Noor <noorsyed1a@gmail.com>
2020-12-04 02:17:55 -0500

    really bad time selector

--- src/viewPage/available.js
+++ src/viewPage/available.js
@@ -12,12 +12,12 @@ let style = {
 export default function AvailabilityTable({ meetingData }) {
     
     const [selecting, setSelecting] = useState(false)
-    const [startpint, setStartpoint] = useState(null)
+    const [startpoint, setStartpoint] = useState(null)
     const [endpoint, setEndpoint] = useState(null)
 
-    function Column({ day, time, availableCount, style }) {
+    function Column({ day, time, availableCount, className }) {
         return (
-            <Col onMouseDown={down} onMouseUp={up} onMouseEnter={hover} id={time + "-" + day} className="table-col" >{availableCount}</Col>
+            <Col onMouseDown={down} onMouseUp={up} onMouseEnter={hover} id={time + "-" + day} className={className} >{availableCount}</Col>
         )
     }
     function hover(data) {
@@ -32,8 +32,9 @@ export default function AvailabilityTable({ meetingData }) {
     function down(data) {
         console.log("down!!!")
         setSelecting(true)
-        setStartpoint(parseId(data.target.id))
-
+        let coords = parseId(data.target.id)
+        setStartpoint(coords)
+        setStartpoint(coords)
     }
     function up(data) {
         console.log("up!!!")
@@ -45,10 +46,20 @@ export default function AvailabilityTable({ meetingData }) {
         console.log("saving changes")
     }
     function parseId(id) {
-        return id.split('-')
+        return id.split('-').map(n => parseInt(n))
     }
     useEffect(() => {
-        console.log("use state!",startpint, endpoint)
+        console.log("raw points: ",startpoint, endpoint)
+        if(startpoint != null && endpoint != null) {
+            let topLeft = [Math.min(startpoint[0], endpoint[0]), Math.min(startpoint[1], endpoint[1])]
+            let bottomRight = [Math.max(startpoint[0], endpoint[0]), Math.max(startpoint[1], endpoint[1])]
+            console.log("processed: ",topLeft[0], topLeft[1])
+            for(let row = topLeft[0]; row <= bottomRight[0]; row++) {
+                for(let col = topLeft[1]; col <= bottomRight[1]; col++) {
+                    document.getElementById(row + "-" + col).className = "selected"
+                }
+            }
+        }
     });
 
     let table = []
@@ -62,7 +73,7 @@ export default function AvailabilityTable({ meetingData }) {
             meetingData.people.forEach(person => {
                 availableCount += person.available[index] === '1' ? 1 : 0
             });
-            currRow.push(<Column day={day} time={time} availableCount={availableCount} style={style} />)
+            currRow.push(<Column day={day} time={time} availableCount={availableCount} className = "table-col" />)
         }
         table.push(<Row>{currRow}</Row>)
     }
